90d8509cca21e064b26e1fdb2b71cc157f0601f3
Evgeny Boger <boger@contactless.ru>
2015-11-05 15:51:58 +0300

    leds: leds-gpio: support LEDs connected in open-drain mode
    Support LEDs connected with external pull-up.
    It may be useful to use the LED to indicate power supply presense and to control
    the same LED from Linux.

--- drivers/leds/leds-gpio.c
+++ drivers/leds/leds-gpio.c
@@ -28,9 +28,25 @@ struct gpio_led_data {
 	u8 new_level;
 	u8 can_sleep;
 	u8 blinking;
+	u8 open_drain;
 	int (*platform_gpio_blink_set)(struct gpio_desc *desc, int state,
 			unsigned long *delay_on, unsigned long *delay_off);
 };
+
+static void gpio_led_set_value(const struct gpio_led_data *led_dat, int value, int can_sleep)
+{
+	if (led_dat->open_drain) {
+		if (value)
+			gpiod_direction_input(led_dat->gpiod);
+		else
+			gpiod_direction_output(led_dat->gpiod, 0);
+	} else {
+		if (can_sleep)
+			gpiod_set_value_cansleep(led_dat->gpiod, value);
+		else
+			gpiod_set_value(led_dat->gpiod, value);
+	}
+}
 
 static void gpio_led_work(struct work_struct *work)
 {
@@ -42,7 +58,7 @@ static void gpio_led_work(struct work_struct *work)
 					led_dat->new_level, NULL, NULL);
 		led_dat->blinking = 0;
 	} else
-		gpiod_set_value_cansleep(led_dat->gpiod, led_dat->new_level);
+		gpio_led_set_value(led_dat, led_dat->new_level, 1);
 }
 
 static void gpio_led_set(struct led_classdev *led_cdev,
@@ -70,7 +86,7 @@ static void gpio_led_set(struct led_classdev *led_cdev,
 							 NULL, NULL);
 			led_dat->blinking = 0;
 		} else
-			gpiod_set_value(led_dat->gpiod, level);
+			gpio_led_set_value(led_dat, level, 0);
 	}
 }
 
@@ -123,6 +139,7 @@ static int create_gpio_led(const struct gpio_led *template,
 
 	led_dat->cdev.name = template->name;
 	led_dat->cdev.default_trigger = template->default_trigger;
+	led_dat->open_drain = template->open_drain;
 	led_dat->can_sleep = gpiod_cansleep(led_dat->gpiod);
 	led_dat->blinking = 0;
 	if (blink_set) {
@@ -138,9 +155,11 @@ static int create_gpio_led(const struct gpio_led *template,
 	if (!template->retain_state_suspended)
 		led_dat->cdev.flags |= LED_CORE_SUSPENDRESUME;
 
-	ret = gpiod_direction_output(led_dat->gpiod, state);
-	if (ret < 0)
-		return ret;
+	if (!led_dat->open_drain) {
+		ret = gpiod_direction_output(led_dat->gpiod, state);
+		if (ret < 0)
+			return ret;
+	}
 
 	INIT_WORK(&led_dat->work, gpio_led_work);
 
@@ -216,6 +235,8 @@ static struct gpio_leds_priv *gpio_leds_create(struct platform_device *pdev)
 
 		if (fwnode_property_present(child, "retain-state-suspended"))
 			led.retain_state_suspended = 1;
+		if (fwnode_property_present(child, "open-drain"))
+			led.open_drain = 1;
 
 		ret = create_gpio_led(&led, &priv->leds[priv->num_leds++],
 				      dev, NULL);
