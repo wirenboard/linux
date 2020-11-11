4a03f312e59ef05fa31c2683fda745532976bab1
Ivan Zaentsev <ivan.zaentsev@wirenboard.ru>
2020-11-11 20:11:30 +0300

    TODO: Squash defconf.sh

--- defconf.sh
+++ defconf.sh
@@ -5,6 +5,18 @@ die() { echo -e "\e[31m$*\e[0m"; exit 1; }
 try() { "$@" || die "cannot $*"; }
 set -x
 
-if [ $1 ]; then KERNEL_DEFCONFIG=$1; else KERNEL_DEFCONFIG=imx6_wirenboard_defconfig; fi
+if [ $1 ]; then 
+    case "$1" in
+        "wb5")
+	KERNEL_DEFCONFIG=mxs_wirenboard_defconfig;
+        ;;
+    "wb6")
+	KERNEL_DEFCONFIG=imx6_wirenboard_defconfig;
+        ;;
+    *)
+        KERNEL_DEFCONFIG=$1;
+        ;;
+    esac
+else KERNEL_DEFCONFIG=imx6_wirenboard_defconfig; fi
 
 try make -j8 ARCH=arm LOCALVERSION=ivz $KERNEL_DEFCONFIG
