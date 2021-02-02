eb6ce32db6bf870ae4639ccc6be9e80125e783dd
Ivan Zaentsev <ivan.zaentsev@wirenboard.ru>
2021-02-02 19:16:26 +0300

    w1: w1_io.c: Reduce bus delays magic numbers
    Bus timing shows that there is a significant software delay
    in bit-banging the w1 protocol for w1_gpio master.
    
    Reduce the magic numbers (delays) to meet the w1 specification.

--- drivers/w1/w1_io.c
+++ drivers/w1/w1_io.c
@@ -164,9 +164,9 @@ static u8 w1_read_bit(struct w1_master *dev)
 	/* sample timing is critical here */
 	local_irq_save(flags);
 	dev->bus_master->write_bit(dev->bus_master->data, 0);
-	w1_delay(6);
+	w1_delay(5);
 	dev->bus_master->write_bit(dev->bus_master->data, 1);
-	w1_delay(9);
+	w1_delay(5);
 
 	result = dev->bus_master->read_bit(dev->bus_master->data);
 	local_irq_restore(flags);
