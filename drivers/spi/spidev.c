d5418824e4d901cb732091b31d94ffb4033244cd
Maslov Nikita <webconn@users.noreply.github.com>
2022-01-28 13:44:56 +0300

    spidev: add compatible for rfm69w (#73)

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -691,6 +691,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "lwn,bk4" },
 	{ .compatible = "dh,dhcom-board" },
 	{ .compatible = "menlo,m53cpld" },
+	{ .compatible = "hoperf,rfm69w" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
