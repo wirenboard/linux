600efd4fb1eaceef6e7dbc77dd08bdc0ef3b1be8
Evgeny Boger <boger@wirenboard.com>
2023-02-05 21:31:00 +0300

    add wb7min defconfig
    minimum configuration w/o Wi-Fi and with everything compiled in kernel.

--- scripts/package/wb/version.sh
+++ scripts/package/wb/version.sh
@@ -33,6 +33,11 @@ setup_kernel_vars() {
 			KERNEL_DEFCONFIG=wirenboard7_defconfig
 			KDEB_WBDESC="Wiren Board 7"
 			;;
+		wb7min)
+			DEBARCH=armhf
+			KERNEL_DEFCONFIG=wirenboard7_min_defconfig
+			KDEB_WBDESC="Wiren Board 7 min no modules, no wireless"
+			;;
 		*)
 			echo "Unsupported KERNEL_FLAVOUR, please specify one of: wb2, wb6, wb7"
 			return 1
