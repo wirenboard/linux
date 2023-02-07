e342402c61ea42cd40844aa3f7ff30f4afdefacb
Pavel Gasheev <p.gasheev@yandex.ru>
2023-02-07 13:55:29 +0300

    Add  watchdog driver

---
+++ drivers/watchdog/wdt-wbec.c
@@ -0,0 +1,297 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * TODO
+ * Write description
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/watchdog.h>
+#include <linux/platform_device.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/jiffies.h>
+#include <linux/property.h>
+#include <linux/regmap.h>
+#include <linux/of.h>
+#include "wbec.h"
+
+static const unsigned int wdt_timeout[] = { 0, 2, 4, 8, 16, 32, 65, 131 };
+
+#define WBEC_REG_WDT_CONTROL			0x30
+#define WBEC_REG_WDT_CONTROL_RESET		BIT(0)
+#define WBEC_REG_WDT_CONTROL_STOP		BIT(1)
+// #define DA9062_TWDSCALE_DISABLE		0
+#define WBEC_TWDSCALE_MIN		1
+#define WBEC_TWDSCALE_MAX		(ARRAY_SIZE(wdt_timeout) - 1)
+#define WBEC_WDT_MIN_TIMEOUT		wdt_timeout[WBEC_TWDSCALE_MIN]
+#define WBEC_WDT_MAX_TIMEOUT		wdt_timeout[WBEC_TWDSCALE_MAX]
+#define WBEC_WDG_DEFAULT_TIMEOUT	wdt_timeout[WBEC_TWDSCALE_MAX-1]
+#define WBEC_RESET_PROTECTION_MS	300
+
+struct wbec_watchdog {
+	struct device *dev;
+	struct regmap *regmap;
+	struct watchdog_device wdtdev;
+	bool use_sw_pm;
+};
+
+static unsigned int wbec_wdt_read_timeout(struct wbec_watchdog *wdt)
+{
+	unsigned int val;
+
+	regmap_read(wdt->regmap, WBEC_REG_WDT_CONTROL, &val);
+
+	// TODO Fix units
+	return val;
+}
+
+static unsigned int wbec_wdt_timeout_to_sel(unsigned int secs)
+{
+	// unsigned int i;
+
+	// for (i = DA9062_TWDSCALE_MIN; i <= DA9062_TWDSCALE_MAX; i++) {
+	// 	if (wdt_timeout[i] >= secs)
+	// 		return i;
+	// }
+
+	// return DA9062_TWDSCALE_MAX;
+	return 0;
+}
+
+static int wbec_reset_watchdog_timer(struct wbec_watchdog *wdt)
+{
+	return regmap_update_bits(wdt->regmap, WBEC_REG_WDT_CONTROL,
+				  WBEC_REG_WDT_CONTROL_RESET, 1);
+}
+
+static int wbec_wdt_update_timeout_register(struct wbec_watchdog *wdt,
+					      unsigned int regval)
+{
+	// regmap_update_bits(wdt->regmap,
+	// 			  WBEC_REG_WDT_CONTROL,
+	// 			  DA9062AA_TWDSCALE_MASK,
+	// 			  DA9062_TWDSCALE_DISABLE);
+
+	// usleep_range(150, 300);
+
+	// return regmap_update_bits(chip->regmap,
+	// 			  DA9062AA_CONTROL_D,
+	// 			  DA9062AA_TWDSCALE_MASK,
+	// 			  regval);
+
+	return 0;
+}
+
+static int wbec_wdt_start(struct watchdog_device *wdd)
+{
+	struct wbec_watchdog *wdt = watchdog_get_drvdata(wdd);
+	unsigned int selector;
+	int ret;
+
+	// TODO Remove debug
+	dev_info(wdt->dev, "%s function\n", __func__);
+
+	selector = wbec_wdt_timeout_to_sel(wdt->wdtdev.timeout);
+	ret = wbec_wdt_update_timeout_register(wdt, selector);
+	if (ret)
+		dev_err(wdt->dev, "Watchdog failed to start (err = %d)\n",
+			ret);
+
+	return ret;
+}
+
+static int wbec_wdt_stop(struct watchdog_device *wdd)
+{
+	struct wbec_watchdog *wdt = watchdog_get_drvdata(wdd);
+	int ret;
+
+	// TODO Remove debug
+	dev_info(wdt->dev, "%s function\n", __func__);
+
+	ret = regmap_update_bits(wdt->regmap,
+				 WBEC_REG_WDT_CONTROL,
+				 WBEC_REG_WDT_CONTROL_STOP,
+				 1);
+	if (ret)
+		dev_err(wdt->dev, "Watchdog failed to stop (err = %d)\n",
+			ret);
+
+	return ret;
+}
+
+static int wbec_wdt_ping(struct watchdog_device *wdd)
+{
+	struct wbec_watchdog *wdt = watchdog_get_drvdata(wdd);
+	int ret;
+
+	// TODO Remove debug
+	dev_info(wdt->dev, "%s function\n", __func__);
+
+	ret = wbec_reset_watchdog_timer(wdt);
+	if (ret)
+		dev_err(wdt->dev, "Failed to ping the watchdog (err = %d)\n",
+			ret);
+
+	return ret;
+}
+
+static int wbec_wdt_set_timeout(struct watchdog_device *wdd,
+				  unsigned int timeout)
+{
+	struct wbec_watchdog *wdt = watchdog_get_drvdata(wdd);
+	unsigned int selector;
+	int ret;
+
+	// TODO Remove debug
+	dev_info(wdt->dev, "%s function\n", __func__);
+
+	selector = wbec_wdt_timeout_to_sel(timeout);
+	ret = wbec_wdt_update_timeout_register(wdt, selector);
+	if (ret)
+		dev_err(wdt->dev, "Failed to set watchdog timeout (err = %d)\n",
+			ret);
+	else
+		wdd->timeout = wdt_timeout[selector];
+
+	return ret;
+}
+
+static int wbec_wdt_restart(struct watchdog_device *wdd, unsigned long action,
+			      void *data)
+{
+
+	struct wbec_watchdog *wdt = watchdog_get_drvdata(wdd);
+	// struct i2c_client *client = to_i2c_client(wdt->hw->dev);
+	// int ret;
+
+	// TODO Remove debug
+	dev_info(wdt->dev, "%s function\n", __func__);
+
+	// /* Don't use regmap because it is not atomic safe */
+	// ret = i2c_smbus_write_byte_data(client, WBEC_REG_POWERCTRL,
+	// 				WBEC_REG_BIT_POWEROFF);
+	// if (ret < 0)
+	// 	dev_alert(wdt->hw->dev, "Failed to shutdown (err = %d)\n",
+	// 		  ret);
+
+	// /* wait for reset to assert... */
+	// mdelay(500);
+
+	// return ret;
+	return 0;
+}
+
+static const struct watchdog_info wbec_watchdog_info = {
+	.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING,
+	.identity = "WBEC WDT",
+};
+
+static const struct watchdog_ops wbec_watchdog_ops = {
+	.owner = THIS_MODULE,
+	.start = wbec_wdt_start,
+	.stop = wbec_wdt_stop,
+	.ping = wbec_wdt_ping,
+	.set_timeout = wbec_wdt_set_timeout,
+	.restart = wbec_wdt_restart,
+};
+
+static int wbec_wdt_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	unsigned int timeout;
+	struct wbec *wbec;
+	struct wbec_watchdog *wdt;
+
+	// TODO Remove debug
+	dev_info(&pdev->dev, "%s function\n", __func__);
+
+	wbec = dev_get_drvdata(dev->parent);
+	if (!wbec)
+		return -EINVAL;
+
+	wdt = devm_kzalloc(dev, sizeof(*wdt), GFP_KERNEL);
+	if (!wdt)
+		return -ENOMEM;
+
+	wdt->use_sw_pm = device_property_present(dev, "dlg,use-sw-pm");
+
+	wdt->dev = dev;
+	wdt->regmap = wbec->regmap_8;
+
+	wdt->wdtdev.info = &wbec_watchdog_info;
+	wdt->wdtdev.ops = &wbec_watchdog_ops;
+	wdt->wdtdev.min_timeout = WBEC_WDT_MIN_TIMEOUT;
+	wdt->wdtdev.max_timeout = WBEC_WDT_MAX_TIMEOUT;
+	wdt->wdtdev.min_hw_heartbeat_ms = WBEC_RESET_PROTECTION_MS;
+	wdt->wdtdev.timeout = WBEC_WDG_DEFAULT_TIMEOUT;
+	wdt->wdtdev.status = WATCHDOG_NOWAYOUT_INIT_STATUS;
+	wdt->wdtdev.parent = dev;
+
+	watchdog_set_restart_priority(&wdt->wdtdev, 128);
+
+	watchdog_set_drvdata(&wdt->wdtdev, wdt);
+	dev_set_drvdata(dev, &wdt->wdtdev);
+
+	// timeout = wbec_wdt_read_timeout(wdt);
+	// if (timeout)
+	// 	wdt->wdtdev.timeout = timeout;
+
+	// /* Set timeout from DT value if available */
+	// watchdog_init_timeout(&wdt->wdtdev, 0, dev);
+
+	// if (timeout) {
+	// 	da9062_wdt_set_timeout(&wdt->wdtdev, wdt->wdtdev.timeout);
+	// 	set_bit(WDOG_HW_RUNNING, &wdt->wdtdev.status);
+	// }
+
+	return devm_watchdog_register_device(dev, &wdt->wdtdev);
+}
+
+// static int __maybe_unused da9062_wdt_suspend(struct device *dev)
+// {
+// 	struct watchdog_device *wdd = dev_get_drvdata(dev);
+// 	struct da9062_watchdog *wdt = watchdog_get_drvdata(wdd);
+
+// 	if (!wdt->use_sw_pm)
+// 		return 0;
+
+// 	if (watchdog_active(wdd))
+// 		return da9062_wdt_stop(wdd);
+
+// 	return 0;
+// }
+
+// static int __maybe_unused da9062_wdt_resume(struct device *dev)
+// {
+// 	struct watchdog_device *wdd = dev_get_drvdata(dev);
+// 	struct da9062_watchdog *wdt = watchdog_get_drvdata(wdd);
+
+// 	if (!wdt->use_sw_pm)
+// 		return 0;
+
+// 	if (watchdog_active(wdd))
+// 		return da9062_wdt_start(wdd);
+
+// 	return 0;
+// }
+
+// static SIMPLE_DEV_PM_OPS(da9062_wdt_pm_ops,
+// 			 da9062_wdt_suspend, da9062_wdt_resume);
+
+static struct platform_driver wbec_wdt_driver = {
+	.probe = wbec_wdt_probe,
+	.driver = {
+		.name = "wbec-watchdog",
+		// .pm = &da9062_wdt_pm_ops,
+		// .of_match_table = da9062_compatible_id_table,
+	},
+};
+module_platform_driver(wbec_wdt_driver);
+
+MODULE_AUTHOR("Pavel Gasheev <pavel.gasheev@wirenboard.ru>");
+MODULE_DESCRIPTION("Wiren Board 7 Embedded Controller Watchdog driver");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:wbec-watchdog");
