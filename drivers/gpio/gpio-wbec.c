f83fd09ca3b7fe271833a3fd50858f9667bac46c
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-07 16:35:24 +0300

    Rework gpio driver (incomplete)

--- drivers/gpio/gpio-wbec.c
+++ drivers/gpio/gpio-wbec.c
@@ -12,113 +12,28 @@
 #include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/regmap.h>
+#include <linux/gpio/regmap.h>
 #include "wbec.h"
-
-enum {
-	WBEC_GPIO_V_OUT,
-	WBEC_GPIO_A1,
-	WBEC_GPIO_A2,
-	WBEC_GPIO_A3,
-	WBEC_GPIO_A4,
-
-	WBEC_GPIO_COUNT
-};
-
-struct wbec_gpio_info {
-	struct wbec *wbec;
-	struct regmap *regmap;
-	struct device *dev;
-	struct gpio_chip gpio_chip;
-};
-
-
-/* generic gpio chip */
-static int wbec_gpio_get(struct gpio_chip *chip, unsigned int offset)
-{
-	struct wbec_gpio_info *pci = gpiochip_get_data(chip);
-	int ret, val;
-
-    // TODO Remove debug
-	dev_info(pci->dev, "%s function. offset=%u\n", __func__, offset);
-
-	ret = regmap_read(pci->regmap, WBEC_REG_GPIO, &val);
-	if (ret < 0) {
-		dev_err(pci->dev, "error when read gpio %u\n", offset);
-		return ret;
-	}
-
-	return !!(val & BIT(offset));
-}
-
-static void wbec_gpio_set(struct gpio_chip *chip,
-			   unsigned int offset,
-			   int value)
-{
-	struct wbec_gpio_info *pci = gpiochip_get_data(chip);
-	int ret;
-
-	// TODO Remove debug
-	dev_info(pci->dev, "%s function. offset=%u, value=%d\n", __func__, offset, value);
-
-	ret = regmap_update_bits(pci->regmap, WBEC_REG_GPIO, BIT(offset), value);
-	if (ret < 0)
-		dev_err(pci->dev, "error when set gpio %u\n", offset);
-}
-
-static int wbec_gpio_get_direction(struct gpio_chip *chip, unsigned int offset)
-{
-	switch (offset) {
-	case WBEC_GPIO_V_OUT:
-		return GPIO_LINE_DIRECTION_OUT;
-
-	case WBEC_GPIO_A1:
-	case WBEC_GPIO_A2:
-	case WBEC_GPIO_A3:
-	case WBEC_GPIO_A4:
-		return GPIO_LINE_DIRECTION_IN;
-	}
-
-	return -ENOTSUPP;
-}
-
-static const struct gpio_chip wbec_gpio_chip = {
-	.label = "wbec-gpio",
-	.owner = THIS_MODULE,
-	.get = wbec_gpio_get,
-	.set = wbec_gpio_set,
-	.get_direction = wbec_gpio_get_direction,
-	.can_sleep = true,
-	.base = 0x400,	// TODO must be -1
-	.ngpio = WBEC_GPIO_COUNT,
-};
 
 static int wbec_gpio_probe(struct platform_device *pdev)
 {
-	struct wbec_gpio_info *gpio;
-	int ret;
+	struct gpio_regmap_config config = {};
+	struct wbec *wbec;
 
-	gpio = devm_kzalloc(&pdev->dev, sizeof(*gpio), GFP_KERNEL);
-	if (!gpio)
-		return -ENOMEM;
+	if (!pdev->dev.parent)
+		return -ENODEV;
 
-	platform_set_drvdata(pdev, gpio);
+	wbec = dev_get_drvdata(pdev->dev.parent);
 
-	gpio->dev = &pdev->dev;
-	gpio->wbec = dev_get_drvdata(pdev->dev.parent);
+	config.regmap = wbec->regmap_8;
+	config.parent = &pdev->dev;
+	config.reg_dat_base = WBEC_REG_GPIO;
+	config.reg_set_base = WBEC_REG_GPIO;
+	config.label = "wbec-gpio";
+	config.ngpio_per_reg = 8;
+	config.reg_stride = 1;
 
-	/* Register gpio controller */
-	gpio->gpio_chip = wbec_gpio_chip;
-	gpio->gpio_chip.parent = &pdev->dev;
-	gpio->gpio_chip.of_node = pdev->dev.parent->of_node;
-
-	/* Add gpio chip */
-	ret = devm_gpiochip_add_data(&pdev->dev, &gpio->gpio_chip, gpio);
-	if (ret < 0) {
-		dev_err(&pdev->dev, "Couldn't add gpiochip\n");
-		return ret;
-	}
-
-	return 0;
+	return PTR_ERR_OR_ZERO(devm_gpio_regmap_register(&pdev->dev, &config));
 }
 
 static struct platform_driver wbec_gpio_driver = {
