db94e1f29df4ce6f77f7ecd11a8d3e3759c3d63d
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-03-19 20:42:18 +0500

    wb8 bootlet config (WIP, needs cleanup)

--- scripts/package/wb/version.sh
+++ scripts/package/wb/version.sh
@@ -71,8 +71,17 @@ setup_kernel_vars() {
 			KERNEL_DEFCONFIG=wb8_reduced_defconfig
 			KDEB_WBDESC="Wiren Board 8"
 			;;
+		wb8x-bootlet)
+			INITRAMFS_DIR="/usr/src/wb-initramfs/wb8x-bootlet"
+			DEBARCH=arm64
+			KERNEL_DEFCONFIG=wb8_bootlet_defconfig
+			BOOTLET_DTB=allwinner/sun50i-h616-wirenboard8xx.dtb
+			PROVIDES_BOOTLET_FOR_FITS=y
+			BOOTLET_DEPS=linux-image-wb8
+			KDEB_WBDESC="Wiren Board 8 (bootlet)"
+            ;;
 		*)
-			echo "Unsupported KERNEL_FLAVOUR, please specify one of: wb2, wb6, wb7"
+			echo "Unsupported KERNEL_FLAVOUR, please specify one of: wb2, wb6, wb7, wb8"
 			return 1
 	esac
 
