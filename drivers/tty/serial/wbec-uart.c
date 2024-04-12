2710a293e5bc8f8b29c68a05f85709522ba5db16
Pavel Gasheev <p.gasheev@yandex.ru>
2024-04-12 23:27:57 +0300

    rework logs

--- drivers/tty/serial/wbec-uart.c
+++ drivers/tty/serial/wbec-uart.c
@@ -65,8 +65,9 @@ void wbec_uart_tx_poll_wq(struct work_struct *work)
 	struct uart_port *port = &wbec_uart->port;
 	struct circ_buf *xmit = &wbec_uart->port.state->xmit;
 	union uart_tx tx;
+	char str[256] = "";
 
-	printk(KERN_INFO "%s called\n", __func__);
+	// printk(KERN_INFO "%s called\n", __func__);
 
 	to_send = uart_circ_chars_pending(xmit);
 	to_send = min(to_send, ARRAY_SIZE(tx.bytes_to_send));
@@ -74,13 +75,13 @@ void wbec_uart_tx_poll_wq(struct work_struct *work)
 	tx.bytes_to_send_count = to_send;
 
 	if (to_send) {
-		printk(KERN_INFO "minimum to_send: %d; head: %d; tail: %d\n", to_send, xmit->head, xmit->tail);
+		sprintf(str, "to_send: %d; ", to_send);
 
 		/* Convert to linear buffer */
 		for (i = 0; i < to_send; ++i) {
 			c = xmit->buf[(xmit->tail + i) & (UART_XMIT_SIZE - 1)];
 			tx.bytes_to_send[i] = c;
-			printk(KERN_INFO "char to send: '%c' / 0x%.2X\n", c, c);
+			sprintf(str, "%s[%.2X]", str, c);
 		}
 	}
 	regmap_bulk_write(wbec_uart->regmap, 0x31, tx.buf, 1 + (to_send + 1) / 2);
@@ -90,7 +91,7 @@ void wbec_uart_tx_poll_wq(struct work_struct *work)
 	regmap_read(wbec_uart->regmap, 0x30, &bytes_sent);
 
 	// Bytes actually sent
-	printk(KERN_INFO "bytes actually sent: %d\n", bytes_sent);
+	sprintf(str, "%s; actually sent: %d", str, bytes_sent);
 	port->icount.tx += bytes_sent;
 	xmit->tail = (xmit->tail + bytes_sent) & (UART_XMIT_SIZE - 1);
 
@@ -101,6 +102,7 @@ void wbec_uart_tx_poll_wq(struct work_struct *work)
 		usleep_range(500, 1000);
 		schedule_delayed_work(&wbec_uart->tx_poll, 0);
 	}
+	printk(KERN_INFO "%s\n", str);
 }
 
 static unsigned int wbec_uart_tx_empty(struct uart_port *port)
@@ -143,7 +145,7 @@ static void wbec_uart_stop_tx(struct uart_port *port)
 					      port);
 	printk(KERN_INFO "%s called\n", __func__);
 	cancel_delayed_work_sync(&wbec_uart->tx_poll);
-
+}
 
 static void wbec_uart_stop_rx(struct uart_port *port)
 {
@@ -186,11 +188,17 @@ static void wbec_uart_shutdown(struct uart_port *port)
 static void wbec_uart_set_termios(struct uart_port *port, struct ktermios *termios, struct ktermios *old)
 {
 	printk(KERN_INFO "%s called\n", __func__);
+
+
+
+	uart_update_timeout(port, termios->c_cflag, 115200);
 }
 
 static void wbec_uart_config_port(struct uart_port *port, int flags)
 {
 	printk(KERN_INFO "%s called\n", __func__);
+	if (flags & UART_CONFIG_TYPE)
+		port->type = 123;
 }
 
 static int wbec_uart_verify_port(struct uart_port *port, struct serial_struct *ser)
@@ -228,21 +236,21 @@ static void wbec_uart_rx_work_wq(struct work_struct *work)
 	union uart_rx rx;
 	int i;
 	u8 c;
-	printk(KERN_INFO "%s called\n", __func__);
+	char str[256] = "";
 
 	regmap_bulk_read(wbec_uart->regmap, 0x60, rx.buf, ARRAY_SIZE(rx.buf));
 
-
-	printk(KERN_INFO "received_bytes: %d\n", rx.read_bytes_count);
+	sprintf(str, "received_bytes: %d: ", rx.read_bytes_count);
 	if (rx.read_bytes_count > 0) {
 		for (i = 0; i < rx.read_bytes_count; i++) {
 			c = rx.read_bytes[i];
-			printk(KERN_INFO "byte[%d]: %.2X\n", i, c);
+			sprintf(str, "%s[%.2X]", str, c);
 			wbec_uart->port.icount.rx++;
 			uart_insert_char(&wbec_uart->port, 0, 0, c, TTY_NORMAL);
 		}
 		tty_flip_buffer_push(&wbec_uart->port.state->port);
 	}
+	printk(KERN_INFO "%s\n", str);
 }
 
 static irqreturn_t wbec_uart_irq(int irq, void *dev_id)
@@ -273,6 +281,15 @@ static const struct uart_ops wbec_uart_ops = {
 	.verify_port	= wbec_uart_verify_port,
 	.pm		= wbec_uart_pm,
 };
+
+static int wbec_uart_config_rs485(struct uart_port *port,
+				  struct serial_rs485 *rs485)
+{
+	printk(KERN_INFO "%s called\n", __func__);
+	port->rs485 = *rs485;
+
+	return 0;
+}
 
 static int wbec_uart_probe(struct spi_device *spi)
 {
@@ -337,9 +354,10 @@ static int wbec_uart_probe(struct spi_device *spi)
 	wbec_uart->port.iotype = UPIO_PORT;
 	wbec_uart->port.flags	= UPF_FIXED_TYPE | UPF_LOW_LATENCY;
 	// wbec_uart->port.flags = UPF_BOOT_AUTOCONF;
+	wbec_uart->port.rs485_config = wbec_uart_config_rs485;
 
 	wbec_uart->port.uartclk = 115200;
-	wbec_uart->port.fifosize = 1024;
+	wbec_uart->port.fifosize = 64;
 	wbec_uart->port.line = 0;
 
 	ret = uart_add_one_port(&wbec_uart_driver, &wbec_uart->port);
