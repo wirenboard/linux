0df40e67aa077c3b48f6bb12c9ae56d85dffa40b
Pavel Gasheev <p.gasheev@yandex.ru>
2024-04-17 15:43:34 +0300

    add custom func to read regs

--- drivers/tty/serial/wbec-uart.c
+++ drivers/tty/serial/wbec-uart.c
@@ -11,6 +11,10 @@
 #include <linux/workqueue.h>
 #include <linux/delay.h>
 #include <linux/kthread.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/debugfs.h>
+
 
 #define DRIVER_NAME "wbec-uart"
 
@@ -18,19 +22,99 @@
 #define dev_info(...)
 #define snprintf(...)
 
+#define WBEC_REGMAP_PAD_WORDS_COUNT   		5
+#define WBEC_REGMAP_READ_BIT				BIT(15)
+
+// union spi_word {
+// 	u8 lsb;
+// 	u8 msb;
+// 	u16 word;
+// };
+
+// static void spi_buf_to_u16_words(const u8 *spi_buf, u16 *buf, int len_words)
+// {
+// 	int i;
+// 	for (i = 0; i < len_words; i++) {
+// 		buf[i * 2] = msg[i] & 0xFF;
+// 		buf[i * 2 + 1] = (msg[i] >> 8) & 0xFF;
+// 	}
+// }
+
+static void u16_word_to_spi_buf(u16 word, u8 *buf, int pos_in_words)
+{
+	buf[pos_in_words * 2 + 1] = word & 0xFF;
+	buf[pos_in_words * 2] = (word >> 8) & 0xFF;
+}
+
+static int wbec_read_regs(struct spi_device *spi, u16 addr, u16 *buf, int len)
+{
+	const int transfer_len_words = 1 + WBEC_REGMAP_PAD_WORDS_COUNT + len;
+	const int transfer_len_bytes = transfer_len_words * sizeof(u16);
+	u8 *tx_buf = kmalloc(transfer_len_bytes, GFP_KERNEL);
+	u8 *rx_buf = kmalloc(transfer_len_bytes, GFP_KERNEL);
+	struct spi_transfer xfer = {
+		.tx_buf = tx_buf,
+		.rx_buf = rx_buf,
+		.len = transfer_len_bytes,
+	};
+	int ret, i;
+
+	if (!tx_buf || !rx_buf) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	u16_word_to_spi_buf(addr | WBEC_REGMAP_READ_BIT, tx_buf, 0);
+	for (i = 1; i < transfer_len_words; i++) {
+		u16_word_to_spi_buf(i, tx_buf, i);
+	}
+
+	ret = spi_sync_transfer(spi, &xfer, 1);
+	if (ret < 0) {
+		dev_err(&spi->dev, "Failed to read registers: %d\n", ret);
+		goto out;
+	}
+
+	for (i = 0; i < len; i++) {
+		buf[i] = rx_buf[(1 + WBEC_REGMAP_PAD_WORDS_COUNT) * 2 + i * 2 + 1] |
+			(rx_buf[(1 + WBEC_REGMAP_PAD_WORDS_COUNT) * 2 + i * 2] << 8);
+	}
+
+out:
+	kfree(tx_buf);
+	kfree(rx_buf);
+	return ret;
+}
+
+static bool wbec_regmap_precious(struct device *dev, unsigned int reg)
+{
+	return true;
+}
+
 static const struct regmap_config wbec_regmap_config = {
 	.reg_bits = 16,
 	.read_flag_mask = BIT(7),
 	.val_bits = 16,
 	.max_register = 0xFF,
+	.can_sleep = false,
+	.precious_reg = wbec_regmap_precious,
 };
 
 struct wbec_uart {
 	struct device *dev;
+	struct spi_device *spi;
 	struct uart_port port;
 	struct regmap *regmap;
 	struct delayed_work tx_poll;
 	struct delayed_work rx_work;
+
+	struct dentry *wbec_uart_dir;
+
+	u8 rx_buf_size_stat[400000];
+	int rx_buf_size_stat_idx;
+
+	u8 tx_buf_size_stat[400000];
+	int tx_buf_size_stat_idx;
 };
 
 union uart_tx {
@@ -95,6 +179,10 @@ void wbec_uart_tx_poll_wq(struct work_struct *work)
 	snprintf(str, ARRAY_SIZE(str), "%s; actually sent: %d", str, bytes_sent);
 	port->icount.tx += bytes_sent;
 	xmit->tail = (xmit->tail + bytes_sent) & (UART_XMIT_SIZE - 1);
+
+	if (wbec_uart->tx_buf_size_stat_idx < ARRAY_SIZE(wbec_uart->tx_buf_size_stat)) {
+		wbec_uart->tx_buf_size_stat[wbec_uart->tx_buf_size_stat_idx++] = bytes_sent;
+	}
 
 	if (uart_circ_chars_pending(xmit) < WAKEUP_CHARS)
 		uart_write_wakeup(port);
@@ -167,11 +255,16 @@ static int wbec_uart_startup(struct uart_port *port)
 	struct wbec_uart *wbec_uart = container_of(port,
 					      struct wbec_uart,
 					      port);
-	int rx_buf_size;
+	u16 rx_buf_size;
 	printk(KERN_INFO "%s called\n", __func__);
 
+	wbec_uart->rx_buf_size_stat_idx = 0;
+	wbec_uart->tx_buf_size_stat_idx = 0;
+
+	enable_irq(wbec_uart->port.irq);
 	// flush the RX buffer
-	regmap_read(wbec_uart->regmap, 0x60, &rx_buf_size);
+	// regmap_read(wbec_uart->regmap, 0x60, &rx_buf_size);
+	wbec_read_regs(wbec_uart->spi, 0x60, &rx_buf_size, 1);
 
 	return 0;
 }
@@ -181,9 +274,10 @@ static void wbec_uart_shutdown(struct uart_port *port)
 	struct wbec_uart *wbec_uart = container_of(port,
 					      struct wbec_uart,
 					      port);
-	printk(KERN_INFO "%s called\n", __func__);
+	disable_irq(wbec_uart->port.irq);
 	cancel_delayed_work_sync(&wbec_uart->tx_poll);
 	cancel_delayed_work_sync(&wbec_uart->rx_work);
+	printk(KERN_INFO "%s called\n", __func__);
 }
 
 static void wbec_uart_set_termios(struct uart_port *port, struct ktermios *termios, struct ktermios *old)
@@ -239,7 +333,12 @@ static void wbec_uart_rx_work_wq(struct work_struct *work)
 	u8 c;
 	char str[256] = "";
 
-	regmap_bulk_read(wbec_uart->regmap, 0x60, rx.buf, ARRAY_SIZE(rx.buf));
+	// regmap_bulk_read(wbec_uart->regmap, 0x60, rx.buf, ARRAY_SIZE(rx.buf));
+	wbec_read_regs(wbec_uart->spi, 0x60, rx.buf, ARRAY_SIZE(rx.buf));
+
+	if (wbec_uart->rx_buf_size_stat_idx < ARRAY_SIZE(wbec_uart->rx_buf_size_stat)) {
+		wbec_uart->rx_buf_size_stat[wbec_uart->rx_buf_size_stat_idx++] = rx.read_bytes_count;
+	}
 
 	snprintf(str, ARRAY_SIZE(str), "received_bytes: %d: ", rx.read_bytes_count);
 	if (rx.read_bytes_count > 0) {
@@ -292,10 +391,64 @@ static int wbec_uart_config_rs485(struct uart_port *port,
 	return 0;
 }
 
+
+static ssize_t wbec_uart_rx_stat_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+	struct wbec_uart *wbec_uart = file->private_data;
+	ssize_t ret;
+
+	if (*ppos >= wbec_uart->rx_buf_size_stat_idx)
+		return 0;
+
+	count = min(count, wbec_uart->rx_buf_size_stat_idx - *ppos);
+
+	ret = copy_to_user(buf, &wbec_uart->rx_buf_size_stat[*ppos], count);
+	if (ret)
+		return -EFAULT;
+
+	*ppos += count;
+
+	return count;
+}
+
+static const struct file_operations wbec_uart_rx_stat_fops = {
+	.owner = THIS_MODULE,
+	.read = wbec_uart_rx_stat_read,
+	.open = simple_open,
+};
+
+static ssize_t wbec_uart_tx_stat_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+	struct wbec_uart *wbec_uart = file->private_data;
+	ssize_t ret;
+
+	if (*ppos >= wbec_uart->tx_buf_size_stat_idx)
+		return 0;
+
+	count = min(count, wbec_uart->tx_buf_size_stat_idx - *ppos);
+
+	ret = copy_to_user(buf, &wbec_uart->tx_buf_size_stat[*ppos], count);
+	if (ret)
+		return -EFAULT;
+
+	*ppos += count;
+
+	return count;
+}
+
+static const struct file_operations wbec_uart_tx_stat_fops = {
+	.owner = THIS_MODULE,
+	.read = wbec_uart_tx_stat_read,
+	.open = simple_open,
+};
+
+
 static int wbec_uart_probe(struct spi_device *spi)
 {
-	int ret, wbec_id;
+	int ret;
+	u16 wbec_id;
 	struct wbec_uart *wbec_uart;
+	u16 buf[33];
 
 	printk(KERN_INFO "%s called\n", __func__);
 
@@ -310,22 +463,26 @@ static int wbec_uart_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, wbec_uart);
 
+	wbec_uart->spi = spi;
+
 	wbec_uart->regmap = devm_regmap_init_spi(spi, &wbec_regmap_config);
 	if (IS_ERR(wbec_uart->regmap)) {
 		dev_err(&spi->dev, "regmap initialization failed\n");
 		return PTR_ERR(wbec_uart->regmap);
 	}
 
-	ret = regmap_read(wbec_uart->regmap, 0, &wbec_id);
-	if (ret < 0) {
-		dev_err(&spi->dev, "failed to read the wbec id at 0x0\n");
-		return ret;
-	}
-	ret = regmap_read(wbec_uart->regmap, 0xB0, &wbec_id);
-	if (ret < 0) {
-		dev_err(&spi->dev, "failed to read the wbec id at 0xB0\n");
-		return ret;
-	}
+	// ret = regmap_read(wbec_uart->regmap, 0, &wbec_id);
+	// if (ret < 0) {
+	// 	dev_err(&spi->dev, "failed to read the wbec id at 0x0\n");
+	// 	return ret;
+	// }
+
+	// ret = regmap_bulk_read(wbec_uart->regmap, 0x60, buf, 33);
+	// if (ret < 0) {
+	// 	dev_err(&spi->dev, "failed to read the wbec id at 0xB0\n");
+	// 	return ret;
+	// }
+	ret = wbec_read_regs(spi, 0x00, &wbec_id, 1);
 	dev_info(&spi->dev, "wbec_id 0xB0: %.2X\n", wbec_id);
 
 	// Register the UART driver
@@ -347,6 +504,7 @@ static int wbec_uart_probe(struct spi_device *spi)
 		dev_err(&spi->dev, "Failed to request IRQ: %d\n", ret);
 		return ret;
 	}
+	disable_irq(spi->irq);
 
 
 	// Register the UART port
@@ -370,6 +528,13 @@ static int wbec_uart_probe(struct spi_device *spi)
 		return ret;
 	}
 
+	// init file operations
+	wbec_uart->rx_buf_size_stat_idx = 0;
+	wbec_uart->tx_buf_size_stat_idx = 0;
+	wbec_uart->wbec_uart_dir = debugfs_create_dir("wbec_uart", NULL);
+	debugfs_create_file("rx_buf_size_stat", 0444, wbec_uart->wbec_uart_dir, wbec_uart, &wbec_uart_rx_stat_fops);
+	debugfs_create_file("tx_buf_size_stat", 0444, wbec_uart->wbec_uart_dir, wbec_uart, &wbec_uart_tx_stat_fops);
+
 	printk(KERN_INFO "WBE UART driver loaded\n");
 
 	return 0;
@@ -389,6 +554,8 @@ static int wbec_uart_remove(struct spi_device *spi)
 
 	// Unregister the UART driver
 	uart_unregister_driver(&wbec_uart_driver);
+
+	debugfs_remove_recursive(wbec_uart->wbec_uart_dir);
 
 	return 0;
 }
